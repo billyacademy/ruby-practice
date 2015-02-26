2fc9f5fc7146a4cc45486a303743a89f13f76a05
William Mahoney <wm.j.mahoney@gmail.com>
2015-02-26 16:31:32 -0500

    created a linked list with a node class and a list class

---
+++ linked_list.rb
@@ -0,0 +1,43 @@
+class Node
+  attr_accessor :value
+  attr_accessor :next_node
+  def initialize(value, next_node)
+    @value = value
+    @next_node = next_node
+  end
+end
+
+
+class List
+  attr_accessor :head
+  def initialize(head = nil)
+    @head = head
+  end
+
+  def add(value)
+    @head = Node.new(value, @head)
+  end
+
+  def [](i)
+    count = 0
+    location = @head
+
+    return location if i == 0
+
+    until count == i
+      count += 1
+      location = location.next_node
+    end
+    location
+  end
+end
+
+
+list = List.new
+list.add(1)
+list.add(3)
+list.add(8)
+list.add(9)
+list.add(10)
+
+puts list[2].value
