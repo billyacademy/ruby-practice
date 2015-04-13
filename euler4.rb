65f1e5882a36fc471db1b39a77c71452f8ac3da9
William Mahoney <wm.j.mahoney@gmail.com>
2015-04-13 15:26:31 -0400

    played with benchmarking, using a first hit variable to determine when to stop the loop, this executes slightly faster now

--- euler4.rb
+++ euler4.rb
@@ -1,39 +1,49 @@
-def palindrome?(number)
-  number_string = number.to_s
-  length = number.to_s.length
-  index = 0
-  palindrome_index = length - 1
-  truthiness = true
+require "benchmark"
 
-  while index < length / 2
-    if number_string[index] == number_string[palindrome_index]
-      index += 1
-      palindrome_index -= 1
-    else
-      truthiness = false
-      break
+time = Benchmark.realtime do
+  require 'pry'
+  def palindrome?(number)
+    number_string = number.to_s
+    length = number.to_s.length
+    index = 0
+    palindrome_index = length - 1
+    truthiness = true
+
+    while index < length / 2
+      if number_string[index] == number_string[palindrome_index]
+        index += 1
+        palindrome_index -= 1
+      else
+        truthiness = false
+        break
+      end
     end
+    truthiness
   end
-  truthiness
-end
 
-def largest_palindrome
-  num1 = 999
-  num2 = 999
+  def largest_palindrome
+    num1 = 999
+    num2 = 999
+    first_hit = 0
+    palindrome_array = []
 
-  palindrome_array = []
-
-  while num1 > 0
-    total = num1 * num2
-    if palindrome?(total)
-      palindrome_array << total
-      num1 -= 1
-      num2 = num1
-    else
-      num2 -= 1
+    while num1 > first_hit
+      total = num1 * num2
+      if palindrome?(total)
+        palindrome_array << total
+        first_hit = num2
+        num1 -= 1
+        num2 = num1
+      else
+        num2 -= 1
+      end
     end
+    palindrome_array.max
   end
-  palindrome_array.max
-end
 
-puts largest_palindrome
+  puts largest_palindrome
+
+
+
+end
+puts "Time elapsed #{time*1000} milliseconds"
